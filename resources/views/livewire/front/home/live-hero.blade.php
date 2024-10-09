b790276da77938bca36669dca6b159637089daa5
Mohammad Imani <memeoney026@gmail.com>
2024-10-09 12:42:41 +0330

    updated home page about style updated hero slider

--- resources/views/livewire/front/home/live-hero.blade.php
+++ resources/views/livewire/front/home/live-hero.blade.php
@@ -7,24 +7,26 @@
         </ol>
         <div class="carousel-inner" role="listbox">
           @foreach ($sliders as $key => $slider)
-            <div class="carousel-item {{ $loop->first ? "active" : "" }}">
-                <img src="{{ $slider->getFirstMediaUrl('mainImage') ?: "/Impact/assets/img/carousel-1.jpg" }}" class="img-fluid" alt="{{ $slider->title ?: env('APP_NAME') }}">
-                <div class="carousel-caption">
-                    <div class="p-3" style="max-width: 900px;">
-                        @if ($slider->title)
-                            <h1 class="fs-1 display-1 text-capitalize text-white mb-4">{{ $slider->title }}</h1>
-                        @endif
-                        @if ($slider->description)
-                            <p class="mb-5 fs-5">{!! $slider->description !!}</p>
-                        @endif
-                        @if ($slider->link && $slider->button_title)
-                            <div class="d-flex align-items-center justify-content-center">
-                                <a class="btn btn-hover-bg btn-primary px-2 px-sm-5 py-1 py-sm-3 text-white" href="{{ $slider->link ?: "#about" }}">{{ $slider->button_title }}</a>
-                            </div>
-                        @endif
+            <a href="{{ $slider->link }}">
+                <div class="carousel-item {{ $loop->first ? "active" : "" }}">
+                    <img src="{{ $slider->getFirstMediaUrl('mainImage') ?: "/Impact/assets/img/carousel-1.jpg" }}" class="img-fluid" alt="{{ $slider->title ?: env('APP_NAME') }}">
+                    <div class="carousel-caption">
+                        <div class="p-3" style="max-width: 900px;">
+                            @if ($slider->title)
+                                <h1 class="fs-1 display-1 text-capitalize text-white mb-4">{{ $slider->title }}</h1>
+                            @endif
+                            @if ($slider->description)
+                                <p class="mb-5 fs-5">{!! $slider->description !!}</p>
+                            @endif
+                            @if ($slider->link && $slider->button_title)
+                                <div class="d-flex align-items-center justify-content-center">
+                                    <a class="btn btn-hover-bg btn-primary px-2 px-sm-5 py-1 py-sm-3 text-white" href="{{ $slider->link ?: "#about" }}">{{ $slider->button_title }}</a>
+                                </div>
+                            @endif
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
           @endforeach
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
